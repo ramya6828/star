2be77e369cab48b0aa287c55934f64d8137bcc8b
ramya rajan <138853653+ramya6828@users.noreply.github.com>
2024-02-17 20:21:52 +0530

    9.java

---
+++ 9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main12{
+public static void main(String[] args){
+Scanner sc-new Scanner(System.in);
+int nesc.nextInt();
+for(int i=1;i<=n;i++){
+for(int j=1;j<=n;j++){
+if(j<=1){
+System.out.print(j);
+}
+else{ System.out.print(" ");
+}
+for(int j=n;j>=1;j--){
+if(j<=1){
+System.out.print(j);
+else System.out.print(" "); }
+System.out.println();
+}
