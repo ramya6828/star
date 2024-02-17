8e75702c55676a9bdad18580569900c3c62787d8
ramya rajan <138853653+ramya6828@users.noreply.github.com>
2024-02-17 20:13:59 +0530

    3.java

---
+++ 3.java
@@ -0,0 +1,25 @@
+import java.uƟl.*;
+public class PaƩernEg {
+// FuncƟon to demonstrate paƩern
+public staƟc void printPaƩern(int n) {
+int i, j;
+// outer loop to handle rows
+for (i = 0; i < n; i++) {
+// inner loop to print spaces.
+for (j = n - i; j > 1; j--) {
+System.out.print(" ");
+}
+// inner loop to print stars.
+for (j = 0; j <= i; j++) {
+System.out.print("* ");
+}
+// prinƟng new line for each row
+System.out.println();
+}
+}
+// Driver FuncƟon
+public staƟc void main(String args[]) {
+int n = 6;
+printPaƩern(n);
+}
+}
